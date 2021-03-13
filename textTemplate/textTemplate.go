2b517513e35e01d6cca0d755ac7c8982ea5201f4
lengkubpome <lengkubpome@gmail.com>
2021-03-14 00:18:14 +0700

    Add text template

--- textTemplate/textTemplate.go
+++ textTemplate/textTemplate.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
 
 func main() {
-	fmt.Println("hello")
+	filicity := Person{"Filicity", 23}
+	oliver := Person{"Oliver", 24}
+	// fmt.Printf("Hi I'm %s. %d years old \n", filicity.Name, filicity.Age)
+	// fmt.Printf("Hi I'm %s. %d years old \n", oliver.Name, oliver.Age)
+	const greetPerson = `Hi I'm {{.Name}}. {{.Age}} years old {{"\n"}}`
+
+	people := []Person(filicity, oliver)
+
+	greetTemplate, err := template.Must(template.New("greetingFromPerson").Parse(greetPerson))
+	if err != nil {
+		return
+	}
+
+	fmt.Println(greetTemplate.Name())
+	greetTemplate.Execute(os.Stdout, filicity)
+	greetTemplate.Execute(os.Stdout, oliver)
 }
