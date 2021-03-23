9801863cef44beb501d6382d43cd28cb48e3308c
lengkubpome <lengkubpome@gmail.com>
2021-03-23 22:57:22 +0700

    Video 119

--- goroutine/server/main.go
+++ goroutine/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"io"
 	"log"
 	"net"
@@ -21,7 +22,7 @@ func main() {
 			log.Println(err)
 			return
 		}
-		countingDownHandler(conn)
+		go countingDownHandler(conn)
 
 	}
 
@@ -30,18 +31,43 @@ func main() {
 func countingDownHandler(conn net.Conn) {
 	defer func() {
 		io.WriteString(conn, "Your connection will be closed by server")
+
 		conn.Close()
 	}()
-
 	io.WriteString(conn, "Enter number : ")
-	count := 5
+	input := bufio.NewScanner(conn)
+	count := Scan(input)
+	// ให้ใส่ข้อมูลแค่ 1 - 10 พอ
+	if count > 10 {
+		return
+	}
+
 	for {
 		io.WriteString(conn, strconv.Itoa(count)+"\n")
 		time.Sleep(time.Second)
 		count--
 		if count < 0 {
 			io.WriteString(conn, "Enter number : ")
-			break
+			count = Scan(input)
+			if count == 0 {
+				break
+			}
 		}
 	}
+}
+
+func Scan(input *bufio.Scanner) int {
+
+	if ok := input.Scan(); !ok {
+		log.Printf("Cannot scan value from conn")
+		log.Println("Connection is closed by client")
+		return 0
+	}
+	count, err := strconv.Atoi(input.Text())
+	if err != nil {
+		log.Println("Cannot convernt value from Text to int. Value : ", input.Text())
+		return 0
+	}
+	return count
+
 }
--- goroutine/telnet/main.go
+++ goroutine/telnet/main.go
@@ -8,11 +8,18 @@ import (
 )
 
 func main() {
+	log.SetFlags(log.Ltime)
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
-	io.Copy(os.Stdout, conn)
+	log.Println("Connected to Server successfully")
+	
+	// copy conn --> stdout
+	// log.Println("copy from conn to stdout")
+	go io.Copy(os.Stdout, conn) // ส่งค่า conn ให้กับทาง server
+	// log.Println("copy from stdin to conn")
+	io.Copy(conn, os.Stdin) // ส่งค่า input ฝั่ง client ไปยังการเชื่อมต่อ conn
 
 }
